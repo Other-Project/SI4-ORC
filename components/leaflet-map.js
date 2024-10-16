ceb4ffaf189b3a9c6f7eee7800f16105e2820c0e
Evan Galli <evan_g@orange.fr>
2024-10-16 20:12:20 +0200

    Current position

--- components/leaflet-map.js
+++ components/leaflet-map.js
@@ -1,4 +1,5 @@
-import * as leaflet from "/node_modules/leaflet/dist/leaflet-src.esm.js";
+import "/node_modules/leaflet/dist/leaflet.js";
+import "/node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.js";
 
 export class LeafletMap extends HTMLDivElement {
     constructor() {
@@ -10,17 +11,25 @@ export class LeafletMap extends HTMLDivElement {
         style.href = "/node_modules/leaflet/dist/leaflet.css";
         style.rel = "stylesheet";
         shadow.appendChild(style);
+        style = document.createElement("link");
+        style.href = "/node_modules/leaflet.locatecontrol/dist/L.Control.Locate.css";
+        style.rel = "stylesheet";
+        shadow.appendChild(style);
 
         let mapDiv = document.createElement("div");
         mapDiv.style.height = "100%";
-        let map = leaflet.map(mapDiv, {
+        let map = L.map(mapDiv, {
             zoomControl: false
         }).setView([43.6155, 7.0719], 16);
         setTimeout(() => map.invalidateSize(), 500);
-        leaflet.control.zoom({
+        L.control.zoom({
             position: "bottomright"
         }).addTo(map);
-        leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        L.control.locate({
+            position: "bottomright"
+        }).addTo(map);
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
             maxZoom: 19,
             minZoom: 6,
             attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
