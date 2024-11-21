c43eabb3024beeded75c6ea1642637794971800f
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-11-21 09:46:55 +0100

    Don't go to station if the arrival is near

--- RoutingService/Service.cs
+++ RoutingService/Service.cs
@@ -8,27 +8,31 @@ namespace RoutingService;
 public class Service : IService
 {
     private static readonly HttpClient Client = new();
-    private readonly JcDecauxClient jcDecauxClient = new(Client);
-    private readonly OrsClient orsClient = new(Client);
+    private readonly JcDecauxClient _jcDecauxClient = new(Client);
+    private readonly OrsClient _orsClient = new(Client);
 
 
-    public async Task<IEnumerable<RouteSegment>?> CalculateRoute(double startLon, double startLat, double endLon, double endLat)
+    public async Task<IEnumerable<RouteSegment>?> CalculateRoute(double startLon, double startLat, double endLon,
+        double endLat)
     {
         try
         {
             var start = new GeoCoordinate(startLat, startLon);
             var end = new GeoCoordinate(endLat, endLon);
 
-            await jcDecauxClient.RetrieveContractsAsync();
-            await jcDecauxClient.RetrieveStationsAsync();
-            var startStation = jcDecauxClient.FindNearestStation(start);
+            await _jcDecauxClient.RetrieveContractsAsync();
+            await _jcDecauxClient.RetrieveStationsAsync();
+            var startStation = _jcDecauxClient.FindNearestStation(start);
             if (startStation is null) return null;
-            var endStation = jcDecauxClient.FindNearestStation(end);
+            var endStation = _jcDecauxClient.FindNearestStation(end);
             if (endStation is null) return null;
 
-            var route = await orsClient.GetRoute(start, startStation.Position, OrsClient.Vehicle.FootWalking);
-            route.AddRange(await orsClient.GetRoute(startStation.Position, endStation.Position));
-            route.AddRange(await orsClient.GetRoute(endStation.Position, end, OrsClient.Vehicle.FootWalking));
+            if (start.GetDistanceTo(end) <= start.GetDistanceTo(startStation.Position))
+                return await _orsClient.GetRoute(start, end, OrsClient.Vehicle.FootWalking);
+
+            var route = await _orsClient.GetRoute(start, startStation.Position, OrsClient.Vehicle.FootWalking);
+            route.AddRange(await _orsClient.GetRoute(startStation.Position, endStation.Position));
+            route.AddRange(await _orsClient.GetRoute(endStation.Position, end, OrsClient.Vehicle.FootWalking));
             return route;
         }
         catch (Exception e)
