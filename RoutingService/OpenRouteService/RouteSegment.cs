f7e81f9ef1d5445d201e4da39e017ffc2678517e
Alban FALCOZ <alban.falcoz@orange.fr>
2024-11-25 18:33:34 +0100

    Reverted some useless changes

--- RoutingService/OpenRouteService/RouteSegment.cs
+++ RoutingService/OpenRouteService/RouteSegment.cs
@@ -1,15 +1,14 @@
-﻿using System.Text.Json.Serialization;
-using PolylineEncoder.Net.Models;
+﻿using PolylineEncoder.Net.Models;
 
 namespace RoutingService.OpenRouteService;
 
 public class RouteSegment
 {
-    [JsonPropertyName("RoadName")] public string RoadName { get; set; } = "";
-    [JsonPropertyName("Vehicle")] public OrsClient.Vehicle Vehicle { get; set; }
-    [JsonPropertyName("InstructionText")] public string InstructionText { get; set; } = "";
-    [JsonPropertyName("InstructionType")] public Step.InstructionType InstructionType { get; set; }
-    [JsonPropertyName("Distance")] public double Distance { get; set; }
-    [JsonPropertyName("Duration")] public double Duration { get; set; }
-    [JsonPropertyName("Points")] public IGeoCoordinate[] Points { get; set; } = [];
+    public string RoadName { get; set; } = "";
+    public OrsClient.Vehicle Vehicle { get; set; }
+    public string InstructionText { get; set; } = "";
+    public Step.InstructionType InstructionType { get; set; }
+    public double Distance { get; set; }
+    public double Duration { get; set; }
+    public IGeoCoordinate[] Points { get; set; } = [];
 }
